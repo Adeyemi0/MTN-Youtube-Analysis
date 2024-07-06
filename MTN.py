6b83c724776da6f1de31bf90d5dca74ef1b0f2a9
Adediran Adeyemi <65359919+Adeyemi0@users.noreply.github.com>
2024-07-06 23:25:39 +0100

    Update MTN.py

--- MTN.py
+++ MTN.py
@@ -20,11 +20,8 @@ import warnings
 warnings.filterwarnings('ignore')
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
-try:
-    nlp = spacy.load("en_core_web_sm")
-except OSError:
-    os.system("python -m spacy download en_core_web_sm")
-    nlp = spacy.load("en_core_web_sm")
+
+nlp = spacy.load("en_core_web_sm")
 
 # Download NLTK stopwords
 nltk.download('stopwords')
